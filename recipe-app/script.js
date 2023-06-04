7a99e8f470e4a024c92b96a22c813be91fc14f8d
Siva kumar <59410636+Sivakumar001@users.noreply.github.com>
2023-06-04 18:21:38 +0530

    recipe app on build

---
+++ recipe-app/script.js
@@ -0,0 +1,12 @@
+async function getRandomMeal(){
+    const randomMeal = await fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+    console.log(randomMeal)
+}
+
+async function getMealById(id){
+    const meal = await fetch("https://www.themealdb.com/api/json/v1/1/lookup.php?i="+id)
+}
+
+async function getMealsBySearch(term){
+    const meals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s="+term)
+}
